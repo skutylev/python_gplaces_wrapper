e2e992d810509b518972c21f858776d095fb9f6b
skutylev <skutylev@hse.ru>
2016-01-09 21:24:02 +0300

    add setup.py file for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='python_gplaces_wrapper',
+        version='0.0.1',
+        packages=['wrapper', 'wrapper.tests'],
+        url='https://github.com/skutylev/python_gplaces_wrapper',
+        license='MIT',
+        author='Sergey A. Kutylev',
+        author_email='sergey@kutylev.com',
+        description='Simple wrapper for google geocoding and google places API.'
+)
